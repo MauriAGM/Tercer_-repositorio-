0689d0dbfab1f99485e4f1afd094aaed5d6e65b8
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-05-29 14:00:56 -0500

    Actualizacion ejercicio 1: agregar validaciones

--- Ejercicio 1.cpp
+++ Ejercicio 1.cpp
@@ -3,12 +3,30 @@ using namespace std;
 
 int main() {
     float n1, n2, n3, promedio;
-    cout << "Ingrese las tres calificaciones: ";
-    cin >> n1 >> n2 >> n3;
+    cout << "Ingrese la primera calificacion (0 a 100): ";
+    cin >> n1;
+    if (n1 < 0 || n1 > 100) {
+        cout << "Calificacion invalida." << endl;
+        return 1;
+    }
+    cout << "Ingrese la segunda calificacion (0 a 100): ";
+    cin >> n2;
+    if (n2 < 0 || n2 > 100) {
+        cout << "Calificacion invalida." << endl;
+        return 1;
+    }
+    cout << "Ingrese la tercera calificacion (0 a 100): ";
+    cin >> n3;
+    if (n3 < 0 || n3 > 100) {
+        cout << "Calificacion invalida." << endl;
+        return 1;
+    }
     promedio = (n1 + n2 + n3) / 3;
+
     if (promedio >= 70)
         cout << "Aprueba" << endl;
     else
         cout << "Reprueba" << endl;
     return 0;
 }
+
