b2c87826a1fd6cf322ea7477831bfd2fefe065e8
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-05-30 22:18:21 -0500

    Actualizacion ejercicio 10 : agregar validaciones

--- Ejercicio 10.cpp
+++ Ejercicio 10.cpp
@@ -5,9 +5,15 @@ int main() {
     int anio;
     cout << "Ingrese el anio: ";
     cin >> anio;
+    if (anio <= 0) {
+    	cout << "Anio invalido." << endl;
+        return 1;
+	}
+	
     if ((anio % 4 == 0 && anio % 100 != 0) || (anio % 400 == 0))
         cout << "Es bisiesto" << endl;
     else
         cout << "No es bisiesto" << endl;
     return 0;
+    
 }
