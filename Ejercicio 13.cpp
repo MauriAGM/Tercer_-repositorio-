e5846c45631ef669cc51390ad71ef424c8b4c733
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-05-31 23:32:24 -0500

    Actualizacion ejercicio 13: agregar validaciones

--- Ejercicio 13.cpp
+++ Ejercicio 13.cpp
@@ -5,9 +5,13 @@ int main() {
     float a, b, c;
     cout << "Ingrese tres longitudes: ";
     cin >> a >> b >> c;
-    if (a < b + c && b < a + c && c < a + b)
-        cout << "Forman un triangulo" << endl;
-    else
-        cout << "No forman un triangulo" << endl;
-    return 0;
-}
+    if (a > 0 && b > 0 && c > 0){
+    	if (a < b + c && b < a + c && c < a + b)
+        	cout << "Forman un triangulo" << endl;
+   	 	else
+        	cout << "No forman un triangulo" << endl;
+	} else {
+		cout << "Las longitudes deben ser mayores que 0" << endl;
+	}
+    	return 0;
+	}
