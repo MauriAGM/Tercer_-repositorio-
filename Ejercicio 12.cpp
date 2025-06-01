5380ea9c9fcb1ffbc1187ea64b1f712134ecf752
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-05-31 23:31:44 -0500

    Actualizacion ejercicio 12: agregar iteraciones

--- Ejercicio 12.cpp
+++ Ejercicio 12.cpp
@@ -2,15 +2,20 @@
 using namespace std;
 
 int main() {
+	int repeticiones;
     float x, fx;
-    cout << "Ingrese valor de x: ";
-    cin >> x;
-    if (x < 0)
-        fx = x * x + 2 * x + 3;
-    else if (x == 0)
-        fx = 0;
-    else
+    cout << "Cuantos valores de x desea evaluar: ";
+    cin >> repeticiones; 
+    for (int i = 1; i <= repeticiones; i++){
+    	cout << "Ingrese el valor de x " << i << ": ";
+    	cin >> x;
+    	if (x < 0)
+        	fx = x * x + 2 * x + 3;
+    	else if (x == 0)
+        	fx = 0;
+    	else
         fx = x * x + 2 * x - 3;
-    cout << "f(x) = " << fx << endl;
+        cout << "f(x) = " << fx << endl;
+	}
     return 0;
 }
