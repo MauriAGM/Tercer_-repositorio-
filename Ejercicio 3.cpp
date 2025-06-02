3775c97dd15a4bf0eecf425d482a84e5a0b32818
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-02 13:14:03 -0500

    Actualizacion ejercicio : agregar validacione

--- Ejercicio 3.cpp
+++ Ejercicio 3.cpp
@@ -4,12 +4,18 @@ using namespace std;
 int main() {
     int camisas;
     float precio, total;
-    cout << "Ingrese el numero de camisas y el precio unitario: ";
-    cin >> camisas >> precio;
-    if (camisas >= 3)
-        total = camisas * precio * 0.8;
-    else
-        total = camisas * precio * 0.9;
-    cout << "Total a pagar: " << total << endl;
+    cout << "Ingrese el numero de camisas: ";
+    cin >> camisas;
+	cout << "Ingrese el precio unitario: ";
+    cin >> precio;
+    if (camisas > 0 && precio > 0) {
+		if (camisas >= 3)
+        	total = camisas * precio * 0.8;
+    	else
+        	total = camisas * precio * 0.9;
+    		cout << "Total a pagar: " << total << endl;	
+	} else {
+		cout << "Cantidad o precio invalido" << endl;
+	}
     return 0;
 }
--- Ejercicio 4.cpp
+++ Ejercicio 4.cpp
@@ -4,12 +4,19 @@ using namespace std;
 int main() {
     float compra, total;
     int numero;
-    cout << "Ingrese el monto de la compra y el numero aleatorio: ";
-    cin >> compra >> numero;
-    if (numero < 74)
-        total = compra * 0.85;
-    else
-        total = compra * 0.8;
-    cout << "Total con descuento: " << total << endl;
-    return 0;
+    cout << "Ingrese el monto de la compra: ";
+	cin >> compra;
+	cout << "Ingrese el numero aleatorio: ";
+    cin >> numero;
+    if (compra > 0 && numero >= 0 && numero <=100){
+    	if (numero < 74){
+    		total = compra * 0.85;
+		}else{
+        	total = compra * 0.8;
+    		cout << "Total con descuento: " << total << endl;
+		}	
+	} else {
+		cout << "Datos invalidos" << endl;
+	}
+	return 0;
 }
