047ecf05a26b8e42d6269d48dd681c948b20bc1c
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-02 13:12:09 -0500

    Actualizacion ejercicio 6 : agregar validaciones

--- Ejercicio 6.cpp
+++ Ejercicio 6.cpp
@@ -3,12 +3,18 @@ using namespace std;
 
 int main() {
     float promedio, pension, total;
-    cout << "Ingrese promedio y pension: ";
-    cin >> promedio >> pension;
-    if (promedio >= 18)
-        total = pension * 0.7;
-    else
-        total = pension * 1.18;
-    cout << "Monto a pagar: " << total << endl;
+    cout << "Ingrese promedio: ";
+	cin >> promedio;
+	cout << "Ingrese pension: ";
+    cin >> pension;
+    if(promedio >= 0 && pension >=0 ){
+    	if (promedio >= 18)
+        	total = pension * 0.7;
+    	else
+        	total = pension * 1.18;
+    		cout << "Monto a pagar: " << total << endl;
+	} else {
+		cout << "Datos invalidos" << endl;
+	}
     return 0;
 }
