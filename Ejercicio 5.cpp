bed6b7caf79816a2c893b226921bb8e01780f1e8
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-02 13:12:58 -0500

    Actualizacion ejercicio 5 : mejora y agregar validaciones

--- Ejercicio 5.cpp
+++ Ejercicio 5.cpp
@@ -5,12 +5,18 @@ int main() {
     int edad;
     char sexo;
     float pulsaciones;
-    cout << "Ingrese edad y sexo (M/F): ";
-    cin >> edad >> sexo;
-    if (sexo == 'F' || sexo == 'f')
-        pulsaciones = (220 - edad) / 10.0;
-    else
-        pulsaciones = (210 - edad) / 10.0;
-    cout << "Pulsaciones: " << pulsaciones << endl;
+    cout << "Ingrese edad: ";
+	cin >> edad;
+	cout << "Ingrese sexo (M/F): ";
+    cin >> sexo;
+    if (edad > 0 && (sexo == 'F' || sexo == 'f' || sexo == 'M' || sexo == 'm')){
+    	if (sexo == 'F' || sexo == 'f')
+    	    pulsaciones = (220 - edad) / 10.0;
+   	 	else
+     	   	pulsaciones = (210 - edad) / 10.0;
+			cout << "Pulsaciones: " << pulsaciones << endl;
+	} else {
+		cout << "Datos invalidos " << endl;
+	}
     return 0;
 }
