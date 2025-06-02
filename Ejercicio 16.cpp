f642ead7882f095aae79ee7b224a754e30df43b8
MauriAGM <gutierrezmusajamauricio@gmail.com>
2025-06-02 13:08:49 -0500

    Actualizacion ejercicio 16: agregar validaciones

--- Ejercicio 16.cpp
+++ Ejercicio 16.cpp
@@ -4,8 +4,17 @@ using namespace std;
 int main() {
     int tipo;
     float credito, nuevo;
-    cout << "Ingrese tipo de tarjeta y credito actual: ";
-    cin >> tipo >> credito;
+    cout << "Ingrese tipo de tarjeta (1,2,3 u otro): ";
+    cin >> tipo;
+    if (tipo < 1) {
+    	cout << "Tipo no valido. Se aplicara aumento por defecto(50%) " << endl;
+	}
+	cout << "Ingrese credito actual: ";
+	cin >> credito;
+	if (credito < 0){
+		cout << "Credito invalido. Debe ser mayor o igual a 0 " << endl;
+		return 1;
+	}
     if (tipo == 1)
         nuevo = credito * 1.25;
     else if (tipo == 2)
